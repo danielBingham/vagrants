18884acdf8d35711c0db8b9da0e0761d4af4c684
Daniel Bingham <dbingham@theroadgoeson.com>
2016-09-01 10:55:42 -0400

    Add the vagrant user to the www-data group so we can modify the source directory.

--- rails/cookbooks/rails_vagrant/recipes/virtual_host.rb
+++ rails/cookbooks/rails_vagrant/recipes/virtual_host.rb
@@ -4,6 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+# Append the vagrant user to the www-data group. This will allow us to modify
+# the source directory when we're sshed into the vagrant.   
+group 'www-data' do
+    action :modify
+    members 'vagrant'
+    append  true
+end
+
 template '/etc/apache2/sites-available/learnrails.local.conf' do
     source  'virtual_host.conf.erb'
     owner   'root'
