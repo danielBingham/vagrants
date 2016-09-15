104ee1343c7dc90c65eb0c4708704f237f23d8cf
Daniel Bingham <dbingham@theroadgoeson.com>
2016-09-15 13:55:26 -0400

    Adding a rails/mysql vagrant pulled from foresttofarm.org.

---
+++ rails-mysql/cookbooks/rails_vagrant/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: rails_vagrant
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+include_recipe  'apt::default'
+include_recipe  'rails_vagrant::ruby'
+include_recipe  'rails_vagrant::apache2'
+include_recipe  'rails_vagrant::database'
+include_recipe  'rails_vagrant::rails'
+include_recipe  'rails_vagrant::configuration'
+include_recipe  'rails_vagrant::database_schema'
+include_recipe  'rails_vagrant::virtual_host'
+# If you're using Grunt as a task runner, uncomment the
+# lines below to install it and run npm install.
+# include_recipe  'nodejs'
+# include_recipe  'rails_vagrant::grunt'
